0859ed8f4d6eb5e692877277bca99b49bb1dd55c
DANIEL REZENDE <danirezende@terra.com.br>
2020-05-16 21:22:30 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const robots = {
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
-  state: require('./robots/state.js')//,
-  //image: require('./robots/image.js'),
+  state: require('./robots/state.js'),
+  image: require('./robots/image.js')//,
   //video: require('./robots/video.js'),
   //youtube: require('./robots/youtube.js')
 }
 
 async function start() {
-  robots.input()
-  await robots.text()
-  //await robots.image()
+  //robots.input()
+  //await robots.text()
+  await robots.image()
   //await robots.video()
   //await robots.youtube()
   
